d1b04fc04a52cf43cd978d3b81d6dcc54471f345
cadu0000 <edukadu1000@gmail.com>
2025-07-09 21:54:33 -0300

    improve transaction creation DTO validation

--- src/modules/transaction/dto/create-transaction.dto.ts
+++ src/modules/transaction/dto/create-transaction.dto.ts
@@ -1,21 +1,24 @@
 import { TransactionType } from '@prisma/client';
 import {
-  IsDateString,
   IsEnum,
   IsNumber,
   IsString,
   MinLength,
+  IsOptional,
 } from 'class-validator';
+import { Type } from 'class-transformer';
 
 export class CreateTransactionDto {
   @IsString({ message: 'Title must be a string' })
   @MinLength(5, { message: 'Title must be at least 5 characters long' })
   title: string;
+  
   @IsString({ message: 'Category must be a string' })
   category: string;
 
-  @IsDateString({}, { message: 'Data must be a valid date string' })
-  data: Date;
+  @IsOptional()
+  @Type(() => Date)
+  data?: Date;
 
   @IsNumber({}, { message: 'Price must be a number' })
   price: number;
